da8e43fe16155de99d745851c645f25780e311e4
Hitesh1326 <shindehitesh26@gmail.com>
2018-07-05 18:55:22 +0530

    Update LoginActivity.java

--- bmi/LoginActivity.java
+++ bmi/LoginActivity.java
@@ -1,10 +1,9 @@
-package com.bmi.hitesh.bmi;
+package com.example.shindehitesh.bmi;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,14 +13,14 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+
     EditText etEmail, etPassword;
-    Button btnForget, btnSignIn,btnSignUp;
-    FirebaseAuth firebaseAuth;
-    FirebaseUser user;
+    Button btnSignIn, btnSignUp, btnForget;
+    FirebaseAuth  firebaseAuth;
+
 
 
     @Override
@@ -35,15 +34,12 @@ public class LoginActivity extends AppCompatActivity {
         btnSignUp = (Button)findViewById(R.id.btnSignUp);
         btnSignIn = (Button)findViewById(R.id.btnSignIn);
         firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        if (firebaseAuth.getCurrentUser() != null) {
+       if (firebaseAuth.getCurrentUser() != null) {
             // User is signed in
-            Intent i = new Intent(LoginActivity.this, BMIActivity.class);
-            startActivity(i);
+            startActivity(new Intent(LoginActivity.this , BMIActivity.class  ));
             finish();
         }
-
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,8 +76,6 @@ public class LoginActivity extends AppCompatActivity {
 
             }});
 
-
-
         btnForget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -95,6 +89,9 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
             }
         });
+
+
+
 
 
     }
