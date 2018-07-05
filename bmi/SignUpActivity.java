ecbbdf8915a89a37888e96d52dd23688bb64d710
Hitesh1326 <shindehitesh26@gmail.com>
2018-07-05 18:59:39 +0530

    Update SignUpActivity.java

--- bmi/SignUpActivity.java
+++ bmi/SignUpActivity.java
@@ -1,10 +1,9 @@
-package com.bmi.hitesh.bmi;
+package com.example.shindehitesh.bmi;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,15 +13,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText etSignUpEmail, etSignUpPassword;
     Button btnRegister;
     FirebaseAuth firebaseAuth;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +62,4 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
-
 }
